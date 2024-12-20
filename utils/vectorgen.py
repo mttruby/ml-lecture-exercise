79653d855c62343d6e1dffed647e1128ede46d9d
Erik Rodner <Erik.Rodner@htw-berlin.de>
2024-12-20 12:45:42 +0100

    Allow different noise types

--- utils/vectorgen.py
+++ utils/vectorgen.py
@@ -1,8 +1,15 @@
 import torch
 
-def synthetic_data(w, b, num_examples):  
+def synthetic_data(w, b, num_examples, noise_std=0.01, noise_type='normal'):  
     """Generate y = Xw + b + noise."""
     X = torch.normal(0, 1, (num_examples, len(w)))
     y = torch.matmul(X, w) + b
-    y += torch.normal(0, 0.01, y.shape)
+    
+    if noise_type == 'normal':
+        y += torch.normal(0, noise_std, y.shape)
+    elif noise_type == 'uniform':
+        y += torch.empty(y.shape).uniform_(-noise_std, noise_std)
+    else:
+        raise ValueError("Invalid noise_type. Supported types are 'normal' and 'uniform'.")
+    
     return X, y.reshape((-1, 1))
